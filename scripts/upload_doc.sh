9fac13a4889618ddfcf95ab40d9b0f8b350ca165
kteza1 <k.teza1@gmail.com>
2016-01-12 17:25:47 +0530

    Travis update

--- scripts/upload_doc.sh
+++ scripts/upload_doc.sh
@@ -2,5 +2,7 @@
 
 if [ "$TRAVIS_PULL_REQUEST" == "false" ] && [ "$TRAVIS_BRANCH" == "master" ] && [ "$TRAVIS_RUST_VERSION" = "stable" ] && [ "$TRAVIS_OS_NAME" = "linux" ]; then
 	echo $TRAVIS_REPO_SLUG
+	#mkdir $HOME/docs
+	#DOCS=$HOME/docs
 	curl https://raw.githubusercontent.com/kteza1/mystuff/master/doc-deploy.sh | bash 
 fi
