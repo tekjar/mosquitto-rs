f2d6a23f16e79fb1822452887c7daf7eda2ab555
kteza1 <k.teza1@gmail.com>
2016-01-12 18:10:49 +0530

    Removing everything

--- scripts/upload_doc.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-if [ "$TRAVIS_PULL_REQUEST" == "false" ] && [ "$TRAVIS_BRANCH" == "master" ] && [ "$TRAVIS_RUST_VERSION" = "stable" ] && [ "$TRAVIS_OS_NAME" = "linux" ]; then
-	curl https://raw.githubusercontent.com/kteza1/mystuff/master/doc-deploy.sh | bash 
-fi
