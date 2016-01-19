d64faf8d018bd3cf2cb7dc375f54cd384a804641
kteza1 <k.teza1@gmail.com>
2016-01-19 16:35:33 +0530

    Removing mosquitto link hard codes

--- mosquitto-sys/build.rs
+++ mosquitto-sys/build.rs
@@ -5,6 +5,6 @@ fn main() {
     let current_dir = PathBuf::from(&env::current_dir().unwrap());
     let out_dir = PathBuf::from(&env::var_os("OUT_DIR").unwrap());
 
-    println!("cargo:rustc-flags=-L {:?} -l mosquitto", out_dir.display());
-    println!("cargo:rustc-link-search=native={}", out_dir.display());
+    //println!("cargo:rustc-flags=-L {:?} -l mosquitto", out_dir.display());
+    //println!("cargo:rustc-link-search=native={}", out_dir.display());
 }
